8ffec020f0b7d4785c6a43c1784f6ed286d61b1f
Abdurrahman Qureshi <rahman.a.qureshi@gmail.com>
2019-02-02 22:55:00 -0500

    update matlab scripts

--- hummingbird_matlab/parse_sim_bag.m
+++ hummingbird_matlab/parse_sim_bag.m
@@ -3,13 +3,23 @@
 %   m - map frame
 %   p - partner frame
 %   b - base_link frame
-filename = '2019-01-27-23-56-50.bag';
+filename = '2019-01-30-19-43-43.bag';
 bag = rosbag(filename);
 tf_select = select(bag, 'Topic', '/tf');
 tf_msgs = readMessages(tf_select);
 %% Model states
+% This retrieves gazebo's simtime and model state.
+% I've found that it's close enough to the map->base_link transformation.
 modelStatesSel = select(bag, 'Topic', '/gazebo/model_states');
 modelStateMsgs = readMessages(modelStatesSel);
+gazeboTimeSel = select(bag, 'Topic', '/clock');
+gazeboTimeMsgs = readMessages(gazeboTimeSel);
+simTime = linspace(gazeboTimeMsgs{1}.Clock_.Sec + gazeboTimeMsgs{1}.Clock_.Nsec/1000000000, ...
+                   gazeboTimeMsgs{end}.Clock_.Sec + gazeboTimeMsgs{end}.Clock_.Nsec/1000000000, ...
+                   length(modelStateMsgs));
+hb_fixed = 2;
+hb = 3;
+[hb_pos, hb_rpy] = processGazeboStates(modelStateMsgs, hb);
 %% map to partner
 mp = get_transforms(tf_msgs, 'map', 'partner');
 [tmp omp Rmp] = processTransforms(mp);
@@ -39,32 +49,85 @@ tag4c_transforms = get_transforms(tf_msgs, 'base_link', 'tag4_corrected');
 [tag4c_time tag4c_pos tag4c_rpy] = processTransforms(tag4c_transforms);
 
 %% Plot above
-plot(tmb, obp_true(:,1))
 hold on
+plot(tmb, obp_true(:,1))
 plot(tbp, obp(:,1))
 plot(tag4c_time, tag4c_pos(:,1))
 legend('True base\_link to partner', 'estimated base\_link to partner', 'base\_link to tag4')
 title('base\_link to partner X offset')
+xlabel('time (s)')
+ylabel('m')
+
 figure
-plot(tmb, obp_true(:,2))
 hold on
+plot(tmb, obp_true(:,2))
 plot(tbp, obp(:,2))
 plot(tag4c_time, tag4c_pos(:,2))
 legend('True base\_link to partner', 'estimated base\_link to partner', 'base\_link to tag4')
 title('base\_link to partner Y offset')
+xlabel('time (s)')
+ylabel('m')
+
 figure
-plot(tmb, obp_true(:,3))
 hold on
+plot(tmb, obp_true(:,3))
 plot(tbp, obp(:,3))
 plot(tag4c_time, tag4c_pos(:,3))
 legend('True base\_link to partner', 'estimated base\_link to partner', 'base\_link to tag4')
 title('base\_link to partner Z offset')
+xlabel('time (s)')
+ylabel('m')
+
 figure
 hold on
 plot(tmb, Rbp_true(:,1))
+plot(tbp, Rbp(:,1))
 plot(tag4c_time, tag4c_rpy(:,1))
 legend('True base\_link to partner', 'estimated base\_link to partner', 'base\_link to tag4')
 title('base\_link to partner roll')
+xlabel('time (s)')
+ylabel('rads')
+%% Plot above on single figure
+figure
+subplot(2,2,1)
+hold on
+plot(tmb, obp_true(:,1))
+plot(tbp, obp(:,1))
+plot(tag4c_time, tag4c_pos(:,1))
+legend('True base\_link to partner', 'estimated base\_link to partner', 'base\_link to tag4')
+title('base\_link to partner X offset')
+xlabel('time (s)')
+ylabel('m')
+
+subplot(2,2,2)
+hold on
+plot(tmb, obp_true(:,2))
+plot(tbp, obp(:,2))
+plot(tag4c_time, tag4c_pos(:,2))
+legend('True base\_link to partner', 'estimated base\_link to partner', 'base\_link to tag4')
+title('base\_link to partner Y offset')
+xlabel('time (s)')
+ylabel('m')
+
+subplot(2,2,3)
+hold on
+plot(tmb, obp_true(:,3))
+plot(tbp, obp(:,3))
+plot(tag4c_time, tag4c_pos(:,3))
+legend('True base\_link to partner', 'estimated base\_link to partner', 'base\_link to tag4')
+title('base\_link to partner Z offset')
+xlabel('time (s)')
+ylabel('m')
+
+subplot(2,2,4)
+hold on
+plot(tmb, Rbp_true(:,3))
+plot(tbp, Rbp(:,3))
+plot(tag4c_time, tag4c_rpy(:,3))
+legend('True base\_link to partner', 'estimated base\_link to partner', 'base\_link to tag4')
+title('base\_link to partner yaw')
+xlabel('time (s)')
+ylabel('rads')
 %% Extract imu data
 imuSel = select(bag,'Topic','/mavros/imu/data');
 imuMsgs = readMessages(imuSel);
@@ -75,4 +138,3 @@ imuAcc = [cellfun(@(m) m.LinearAcceleration.X, imuMsgs),...
 imuAng = [cellfun(@(m) m.AngularVelocity.X, imuMsgs),...
                cellfun(@(m) m.AngularVelocity.Y, imuMsgs),...
                cellfun(@(m) m.AngularVelocity.Z, imuMsgs)];
-
---
+++ hummingbird_matlab/ts_model.m
@@ -0,0 +1,69 @@
+%% Full Equations of model
+syms Ixx Iyy Izz
+syms kt km kl kd
+syms w1 w2
+syms d1 d2
+syms l m
+syms ax ay az
+syms alphax alphay alphaz
+syms g
+syms yaw pitch roll
+syms yaw_dot pitch_dot roll_dot
+syms t
+syms u v w
+
+I = [Ixx 0 0; 0 Iyy 0; 0 0 Izz];
+a = [ax;ay;az];
+alpha = [alphax; alphay; alphaz];
+
+R = rotz(yaw)*roty(pitch)*rotx(roll);
+g_body = R'*[0;0;g];
+
+Force = [kl*(d1*w1^2 + d2*w2^2);...
+          0;...
+          kd*(d1^2*w1^2+d2^2*w2^2) - kt*(w1^2+w2^2)] + m*g_body;
+Torque = [kt*l*(w1^2-w2^2);...
+          km*(d1*w1^2+d2*w2^2);...
+          km*(w1^2-w2^2)];
+      
+equations = [Force == m*a; Torque == I*alpha;]
+unknowns = [w1;w2;d1;d2];
+%% Here we simplify by assuming no drag, no gravity
+syms Ixx Iyy Izz
+syms kt km kl kd
+syms w1 w2
+syms d1 d2
+syms l m
+syms ax ay az
+syms alphax alphay alphaz
+g = 0
+syms yaw pitch roll
+syms yaw_dot pitch_dot roll_dot
+syms t
+syms u v w
+
+I = [Ixx 0 0; 0 Iyy 0; 0 0 Izz];
+a = [ax;ay;az];
+alpha = [alphax; alphay; alphaz];
+
+R = rotz(yaw)*roty(pitch)*rotx(roll);
+g_body = R'*[0;0;g];
+
+Force = [kl*(d1*w1^2 + d2*w2^2);...
+          0;...
+         - kt*(w1^2+w2^2)] + m*g_body;
+Torque = [kt*l*(w1^2-w2^2);...
+          km*(d1*w1^2+d2*w2^2);...
+          km*(w1^2-w2^2)];
+      
+equations = [Force == m*a; Torque == I*alpha;]
+unknowns = [w1;w2;d1;d2];
+
+%%
+syms yaw pitch roll
+syms yaw_dot pitch_dot roll_dot
+syms u v w
+body_rate_equations = [u;v;w] == rotx(roll)'*roty(pitch)'*[0;0;yaw_dot] + rotx(roll)'*[0;pitch_dot;0] + [roll_dot;0;0]
+rpy = [roll_dot;pitch_dot;yaw_dot]
+inverse = solve(body_rate_equations, rpy)
+pretty(simplify(diff(inverse.roll_dot,pitch)))
