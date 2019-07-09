a418c1e0a438f034401166ff571b5b0123cd3114
Abdurrahman Qureshi <rahman.a.qureshi@gmail.com>
2019-07-08 20:02:32 -0400

    don't stream anything under normal mode

--- hummingbird_px4/src/modules/mavlink/mavlink_main.cpp
+++ hummingbird_px4/src/modules/mavlink/mavlink_main.cpp
@@ -2010,34 +2010,8 @@ Mavlink::task_main(int argc, char *argv[])
 
 	switch (_mode) {
 	case MAVLINK_MODE_VICON:
-		printf("mode is vicon\n");
 		break; // send absolutely no data
 	case MAVLINK_MODE_NORMAL:
-		configure_stream("SYS_STATUS", 1.0f);
-		configure_stream("EXTENDED_SYS_STATE", 10.0f);
-		configure_stream("HIGHRES_IMU", 25.0f);
-		configure_stream("ATTITUDE", 50.0f);
-		configure_stream("RC_CHANNELS", 5.0f);
-		configure_stream("SERVO_OUTPUT_RAW_0", 10.0f);
-		configure_stream("ALTITUDE", 50.0f);
-		configure_stream("GPS_RAW_INT", 5.0f);
-		configure_stream("ADSB_VEHICLE", 2.0f);
-		configure_stream("DISTANCE_SENSOR", 25.0f);
-		configure_stream("OPTICAL_FLOW_RAD", 25.0f);
-		//configure_stream("VISION_POSITION_NED", 1.0f);
-		configure_stream("ESTIMATOR_STATUS", 1.0f);
-		configure_stream("NAV_CONTROLLER_OUTPUT", 1.5f);
-		configure_stream("GLOBAL_POSITION_INT", 20.0f);
-		configure_stream("LOCAL_POSITION_NED", 25.0f);
-		configure_stream("POSITION_TARGET_GLOBAL_INT", 10.0f);
-		configure_stream("POSITION_TARGET_LOCAL_NED", 25.0f);
-		configure_stream("ATTITUDE_TARGET", 25.0f);
-		configure_stream("HOME_POSITION", 0.5f);
-		configure_stream("NAMED_VALUE_FLOAT", 1.0f);
-		configure_stream("VFR_HUD", 4.0f);
-		configure_stream("WIND_COV", 1.0f);
-		configure_stream("MANUAL_CONTROL", 25.0f);
-		configure_stream("ESC_RADS", 25.0f);
 		break;
 
 	case MAVLINK_MODE_CUSTOM:
