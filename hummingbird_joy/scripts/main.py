14dabb1a9125514d378d539d65bfb9e8f8dae276
Abdurrahman Qureshi <rahman.a.qureshi@gmail.com>
2019-07-12 17:07:27 -0400

    attempting to debug disarm command failures

---
+++ hummingbird_joy/scripts/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Joy
+from mavros_msgs.srv import CommandLong
+
+service = None
+
+class Buttons:
+    X = 0
+    A = 1
+    B = 2
+    Y = 3
+
+class Command:
+    ARM_DISARM = 400
+
+def joyCallback(data):
+    if data.buttons[Buttons.B]:
+        rospy.loginfo("B button pressed")
+        try:
+            service_stub = rospy.ServiceProxy(service, CommandLong)
+            response = service_stub(False, Command.ARM_DISARM, True, 0, 0, 0, 0, 0, 0, 0)
+            print(response)
+        except rospy.ServiceException, e:
+            rospy.logerr("Service call failed: %s"%e)
+
+def main():
+    rospy.init_node('hummingbird_joy', anonymous=True)
+    global service
+    service = rospy.get_param("~command_service")
+    try:
+        rospy.wait_for_service(service, timeout=5)
+    except rospy.ROSException, e:
+        rospy.logerr("Failed while waiting for service %s", service)
+    rospy.Subscriber("/joy", Joy, joyCallback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
